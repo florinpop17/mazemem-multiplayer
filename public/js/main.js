9f8580ffc1a85e3959da60d304609ed2f3d338b9
Florin Pop <popflorin1705@yahoo.com>
2017-02-06 12:30:54 +0200

    drawMaze if maze on client

--- public/js/main.js
+++ public/js/main.js
@@ -8,10 +8,24 @@ function setup() {
     socket = io.connect();
     socket.on('theMaze', function(_maze){
         maze = _maze; 
+        
+        console.log('got maze / loop');
+        loop();
     });
+    
+    console.log('setup / noLoop');
+    noLoop();
 }
 
 function draw() {
     background(51);
+    
+    
+    if(maze){
+        drawMaze();
+    }
+}
+
+function drawMaze(){
     
 }
