a0333e1fdbffc1117ada355d2a0b61e842ecd645
Florin Pop <popflorin1705@yahoo.com>
2017-02-06 11:22:20 +0200

    basic express/socket.io

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('index.html');
+});
+
+server.listen(PORT, () => {
+    console.log("Server listening on port", PORT);
+});
+
+
+io.sockets.on('connection', (socket) => {
+    connections.push(socket);
+    console.log('Connected: %s sockets connected.', connections.length);
+    
+    //Disconnect
+    socket.on('disconnect', (data) => {
+        connections.splice(connections.indexOf(socket), 1);
+        users = users.filter(user => user.id !== socket.id);
+        console.log('Disconnected: %s sockets connected.', connections.length);
+    });
+});
